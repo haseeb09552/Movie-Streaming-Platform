26c857e2d24b3d01ba9378c4f9b2fe811d303e20
varshayama08 <63184006+varshayama08@users.noreply.github.com>
2021-04-01 00:33:39 +0530

    Add files via upload

--- spring/src/main/java/com/example/controller/LoginController.java
+++ spring/src/main/java/com/example/controller/LoginController.java
@@ -1,29 +1,5 @@
-package com.example.controller;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+package com.example.demo.controller;
 
-import com.example.entities.LoginModel;
-import com.example.services.JwtUtil;
-
-@RestController
 public class LoginController {
-
-	@Autowired
-	private JwtUtil jwtUtil;
-	@Autowired
-	private AuthenticationManager authenticationManager;
-	
-	@PostMapping("/login")
-	public String checkUser(LoginModel authRequest) throws Exception {
-		 try {
-	            authenticationManager.authenticate( new UsernamePasswordAuthenticationToken(authRequest.getEmail(), authRequest.getPassword()));
-	        } catch (Exception ex) {
-	            throw new Exception("inavalid username/password");
-	        }
-	        return jwtUtil.generateToken(authRequest.getEmail());
-	}
-	
+    //@PostMapping("/login")
 }
